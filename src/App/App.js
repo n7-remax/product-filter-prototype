f70341f6cd955614e95c3606858a854eb5c4f3df
N7_Remax <ditritusa@gmail.com>
2020-02-04 20:05:26 +0200

    connect FilterableProductTable in App

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import FilterableProductTable from './FilterableProductTable/FilterableProductTable';
+import PRODUCTS from './constants/constants';
 
 function App() {
   return (
     <div className="App">
+      <FilterableProductTable products={PRODUCTS}/>
     </div>
   );
 }
