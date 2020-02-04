46e7c632ebfab2c62bd4f1f93d806589d46c53ce
N7_Remax <ditritusa@gmail.com>
2020-02-04 21:19:25 +0200

    created methods in FilterableProductTable and pass them to SearchBar module

--- src/App/FilterableProductTable/FilterableProductTable.js
+++ src/App/FilterableProductTable/FilterableProductTable.js
@@ -5,14 +5,29 @@ import ProductTable from './Product/ProductTable'
 class FilterableProductTable extends Component {
     constructor(props) {
         super(props)
-        this.state = { filterText: '', inStockOnly: false }
+        this.state = { filterText: '', inStockOnly: false };
+
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+        this.handleinStockChange = this.handleinStockChange.bind(this);
+
     }
+
+    handleFilterTextChange(filterText) {
+        this.setState({ filterText: filterText })
+    }
+
+    handleinStockChange(inStockOnly) {
+        this.setState({ inStockOnly: inStockOnly })
+    }
+
     render() {
         return (
             <div>
                 <SearchBar
                     filterText={this.state.filterText}
-                    inStockOnly={this.state.inStockOnly} />
+                    inStockOnly={this.state.inStockOnly}
+                    onFilterTextChange={this.handleFilterTextChange} 
+                    onInStockChange={this.handleinStockChange}/>
 
                 <ProductTable
                     products={this.props.products}
